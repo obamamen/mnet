a73d4d16d52a53dba7b93a5c7786861379506d47
obamamen <97452038+obamamen@users.noreply.github.com>
2026-01-06 09:59:19 +0100

    Working socket now in windows

--- main.c
+++ main.c
@@ -8,5 +8,29 @@ int main(void)
         fprintf(stderr,"mnet_initialize() failed\n");
     }
 
+    mnet_sockaddr_in_t host_addr;
+    mnet_socket_t host_socket;
+
+    host_socket = mnet_socket(
+        mnet_af_inet,
+        mnet_sock_stream,
+        mnet_ipproto_default);
+
+    mnet_addr_any_ipv4(
+        &host_addr,
+        2020);
+
+    mnet_bind(host_socket, MNET_SOCKADDR(host_addr), sizeof(host_addr));
+
+    if (mnet_socket_is_valid(host_socket))
+    {
+        printf("valid socket: %llu \n", host_socket);
+    } else
+    {
+        printf("invalid socket: %llu \n", host_socket);
+    }
+
+    mnet_listen(host_socket, 1024);
+
     mnet_cleanup();
 }
--- mnet/mnet.h
+++ mnet/mnet.h
@@ -12,10 +12,30 @@
 #ifndef MNET_MNET_H
 #define MNET_MNET_H
 
+#include <stdint.h>
+
 #include "mnet_os.h"
+#include "mnet_types.h"
 
 int mnet_initialize(void);
 void mnet_cleanup(void);
+
+mnet_socket_t mnet_socket(int domain, int type, int protocol);
+int mnet_close(mnet_socket_t sock);
+int mnet_bind(mnet_socket_t sock, const struct sockaddr* addr, socklen_t addrlen);
+int mnet_listen(mnet_socket_t sock, int backlog);
+mnet_socket_t mnet_accept(mnet_socket_t sock, struct sockaddr* addr, socklen_t* addrlen);
+int mnet_connect(mnet_socket_t sock, const struct sockaddr* addr, socklen_t addrlen);
+
+int mnet_socket_is_valid(mnet_socket_t sock);
+
+int mnet_get_error(void);
+const char* mnet_error_string(int error_code);
+
+int mnet_addr_ipv4(struct sockaddr_in* addr, const char* ip,    uint16_t port);
+int mnet_addr_ipv6(struct sockaddr_in6* addr, const char* ip,   uint16_t port);
+int mnet_addr_any_ipv4(struct sockaddr_in* addr,                uint16_t port);
+int mnet_addr_any_ipv6(struct sockaddr_in6* addr,               uint16_t port);
 
 #endif//MNET_MNET_H
 
@@ -43,6 +63,109 @@ void mnet_cleanup(void)
 #ifdef MNET_WINDOWS
     WSACleanup();
 #endif
+}
+
+mnet_socket_t mnet_socket(int domain, int type, int protocol)
+{
+    return socket(domain, type, protocol);
+}
+
+int mnet_close(mnet_socket_t sock)
+{
+#ifdef MNET_WINDOWS
+    return closesocket(sock);
+#elif defined(MNET_UNIX)
+    return close(sock);
+#endif
+}
+
+int mnet_bind(mnet_socket_t sock, const struct sockaddr *addr, socklen_t addrlen)
+{
+    return bind(sock, addr, addrlen);
+}
+
+int mnet_listen(mnet_socket_t sock, int backlog)
+{
+    return listen(sock, backlog);
+}
+
+mnet_socket_t mnet_accept(mnet_socket_t sock, struct sockaddr *addr, socklen_t *addrlen)
+{
+    return accept(sock, addr, addrlen);
+}
+
+int mnet_connect(mnet_socket_t sock, const struct sockaddr *addr, socklen_t addrlen)
+{
+    return connect(sock, addr, addrlen);
+}
+
+int mnet_socket_is_valid(mnet_socket_t sock)
+{
+    return sock != MNET_INVALID_SOCKET;
+}
+
+int mnet_get_error(void)
+{
+#ifdef MNET_WINDOWS
+    return WSAGetLastError();
+#elif defined(MNET_UNIX)
+    return errno;
+#endif
+}
+
+const char* mnet_error_string(int error_code)
+{
+#ifdef MNET_WINDOWS
+    static char buf[256];
+    FormatMessageA(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+                   NULL, error_code, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+                   buf, sizeof(buf), NULL);
+    return buf;
+#elif defined(MNET_UNIX)
+    return strerror(error_code);
+#endif
+}
+
+int mnet_addr_ipv4(struct sockaddr_in *addr, const char *ip, uint16_t port)
+{
+    if (!addr) return -1;
+
+    memset(addr, 0, sizeof(*addr));
+    addr->sin_family = AF_INET;
+    addr->sin_port = htons(port);
+
+    if (!ip || strcmp(ip, "0.0.0.0") == 0) {
+        addr->sin_addr.s_addr = INADDR_ANY;
+        return 0;
+    }
+
+    return inet_pton(AF_INET, ip, &addr->sin_addr) == 1 ? 0 : -1;
+}
+
+int mnet_addr_ipv6(struct sockaddr_in6 *addr, const char *ip, uint16_t port)
+{
+    if (!addr) return -1;
+
+    memset(addr, 0, sizeof(*addr));
+    addr->sin6_family = AF_INET6;
+    addr->sin6_port = htons(port);
+
+    if (!ip || strcmp(ip, "::") == 0) {
+        addr->sin6_addr = in6addr_any;
+        return 0;
+    }
+
+    return inet_pton(AF_INET6, ip, &addr->sin6_addr) == 1 ? 0 : -1;
+}
+
+int mnet_addr_any_ipv4(struct sockaddr_in *addr, uint16_t port)
+{
+    return mnet_addr_ipv4(addr, NULL, port);
+}
+
+int mnet_addr_any_ipv6(struct sockaddr_in6 *addr, uint16_t port)
+{
+    return mnet_addr_ipv6(addr, NULL, port);
 }
 
 #endif
